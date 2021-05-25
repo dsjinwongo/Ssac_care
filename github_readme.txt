7688faad18a70f0fd7aceeb8fb1f6f0db91847b4
kimdoyeom <kimas3497@gmail.com>
2021-05-25 18:56:48 +0900

    052521_01

---
+++ github_readme.txt
@@ -0,0 +1,8 @@
+1. git init
+2. git remote add origin https://github.com/dsjinwongo/Ssac_care.git
+3. git remote update
+4. git git checkout -t origin/(각자 브랜치)
+5. git pull origin (브랜치명)
+6. git add *
+7. git commit -m '커밋 메세지'
+8. git push origin (브랜치 명)
--- js/main.js
+++ js/main.js
@@ -1,134 +1,34 @@
-(function() {
-  "use strict";
+// $(window).load() -> 페이지 문서가 전부 로드된 시점에서 실행된다.
+$(window).load(function(){
+  var height = window.innerHeight,
+  x= 0, y= height/2,
+  curveX = 0,
+  curveY = 0,
+  targetX = 0,
+  xitteration = 0,
+  yitteration = 0,
+  menuExpanded = false;
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
+  blob = $('#blob'),
+  blobPath = $('#blob-path'),
 
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+  hamburger = $('.hamburger');
 
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
+  $(this).on('mousemove', function(e){
+    x = e.pageX;
 
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-  }
+    y = e.pageY;
+  });
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  //해당 id에 마우스가 올라갈 시 function 작동
+  $('.hamburger, .menu-inner').on('mouseenter', function(){
+    $(this).parent().addClass('expanded'); //둘의 부모 요소인 'menu'에 class expanded를
+    menuExpanded = true;
+  });
 
-  /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '#navbar .nav-link', function(e) {
-      let section = select(this.hash)
-      if (section) {
-        e.preventDefault()
-
-        let navbar = select('#navbar')
-        let header = select('#header')
-        let sections = select('section', true)
-        let navlinks = select('#navbar .nav-link', true)
-
-        navlinks.forEach((item) => {
-          item.classList.remove('active')
-        })
-
-        this.classList.add('active')
-
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile')
-          let navbarToggle = select('.mobile-nav-toggle')
-          navbarToggle.classList.toggle('bi-list')
-          navbarToggle.classList.toggle('bi-x')
-        }
-
-        if (this.hash == '#header') {
-          header.classList.remove('header-top')
-          sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          return;
-        }
-
-        if (!header.classList.contains('header-top')) {
-          header.classList.add('header-top')
-          setTimeout(function() {
-            sections.forEach((item) => {
-              item.classList.remove('section-show')
-            })
-            section.classList.add('section-show')
-
-          }, 350);
-        } else {
-          sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          section.classList.add('section-show')
-        }
-
-        scrollto(this.hash)
-      }
-    }, true)
-
-    /**
-     * Activate/show sections on load with hash links
-     */
-    window.addEventListener('load', () => {
-      if (window.location.hash) {
-        let initial_nav = select(window.location.hash)
-
-        if (initial_nav) {
-          let header = select('#header')
-          let navlinks = select('#navbar .nav-link', true)
-
-          header.classList.add('header-top')
-
-          navlinks.forEach((item) => {
-            if (item.getAttribute('href') == window.location.hash) {
-              item.classList.add('active')
-            } else {
-              item.classList.remove('active')
-            }
-          })
-
-          setTimeout(function() {
-            initial_nav.classList.add('section-show')
-          }, 350);
-
-          scrollto(window.location.hash)
-        }
-      }
-    });
-
-})()
+  //해당 id에 마우스가 제거될 시 function 작동
+  $('.menu-inner').on('mouseleave', function(){
+    menuExpanded = false;
+    $(this).parent().removeClass('expanded'); //expanded class를
+  });
+});
